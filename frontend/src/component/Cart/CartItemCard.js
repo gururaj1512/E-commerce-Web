6c83c5d38d35bd4de5ee388c12ae8c2822851ad3
gururaj1512 <gururajgurram1512@gmail.com>
2024-06-23 00:23:38 +0530

    fix

--- frontend/src/component/Cart/CartItemCard.js
+++ frontend/src/component/Cart/CartItemCard.js
@@ -9,7 +9,7 @@ const CartItemCard = ({ item, deleteCartItems }) => {
                 <div className='flex items-center'>
                     <img src={item.image} alt="" className='w-28 h-20 sm:h-16 sm:w-16 object-cover' />
                     <div className='ml-2'>
-                        <Link className='text-sm xl:text-base sm:text-xs line-clamp-1' to={`http:localhost:3000/product/${item.product}`}>{item.name}</Link>
+                        <Link className='text-sm xl:text-base sm:text-xs line-clamp-1' to={`/product/${item.product}`}>{item.name}</Link>
                         <div className="text-sm xl:text-base font-medium text-main-red">₹{item.price}</div>
                         <p className='cursor-pointer text-xs py-1 rounded-full hover:bg-slate-100 hover:text-main-red hidden sm:inline' onClick={() => deleteCartItems(item.product)}>Remove</p>
                     </div>
